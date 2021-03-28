57e78376ce0a228e90b5d4bc1fc11c3eb43643ee
Tamal2017 <tingangt@gmail.com>
2021-03-28 12:32:29 +0000

    Update about component with data

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  info: Info = new Info();
+  comments: Array<Comment> = [];
+  comment: Comment = new Comment();
+  newComment: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addComment() {
+    if (this.comment.message) {
+      this.comment.date = new Date();
+      this.comments.push({ 
+        message: this.comment.message, 
+        date: this.comment.date 
+      });
+      this.comment.message = '';
+    }
+  }
+}
+
+export class Info {
+  name: string = "Test";
+  email: string = "test@mail.com";
+  phone: string = "00000000";
+}
+
+export class Comment {
+  message: string = '';
+  date: Date = null;
 }
